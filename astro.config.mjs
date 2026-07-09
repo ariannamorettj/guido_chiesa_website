4d3d3d23458ae9925f858b59c42d7c8073badb87
amorettij <arianna.moretti2@studio.unibo.it>
2026-07-09 15:40:46 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ariannamorettj.github.io',
+  base: '/guido_chiesa_website',
   vite: {
     plugins: [tailwindcss()]
   }
